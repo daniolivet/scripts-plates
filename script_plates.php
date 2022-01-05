ebff1f0acb345dd47c90ffc3d76e9a2966cbf173
Daniel Olivet <48984255+daniolivet@users.noreply.github.com>
2022-01-05 14:13:21 +0100

    script_plates.php new version
    This version can process differents license plates. We have 2 options: sector=<number> || pmr=1 (if you want process pmr license plates)

--- script_plates.php
+++ script_plates.php
@@ -1,80 +1,146 @@
-<?php 
+<?php
 
 /* Constans */
 define('NAME_FILE', 'matriculas_nuevas.csv');
 
 class MatriculasCSV {
 
-    protected $sector;
+    protected $pmr = false;
+    protected $sector = 0;
     protected $csv;
+    protected $csv2;
     protected $plates = [];
+    protected $smassa_plates = [];
+    protected $plates_diff = [];
 
-    public function __construct($args = null){
+    public function __construct( Array $args = [] ){
         array_shift($args);
         $cnf = [];
 
-        foreach($args as $param) {
-            
-            list($key,$value) = array_filter(explode('=', $param));
-    
-            if( empty($value) ) {
-                die( $key.' esta vacio.');
-            }
+        $cnf = $this->getCnf($args, $cnf);
 
-            $cnf[$key] = $value;
-        }
-    
-        
-        if( file_exists($cnf['fileCSV']) ){
-            $this->csv = file($cnf['fileCSV']);
+        if( !isset($cnf['fileCSV']) || !isset($cnf['fileCSV2']) ){
+            die("The parametre fileCSV or fileCSV2 don't exists in execution of script.");
         }
 
-        $this->sector = $cnf['sector'];
+        if( isset($cnf['pmr']) ) {
+            $this->pmr = true;
+        }
 
+        if(isset($cnf['sector'])) {
+            $this->sector = $cnf['sector'];
+        }
+
+        $this->csv = file($cnf['fileCSV']);
+        $this->csv2 = file($cnf['fileCSV2']);
     }
 
-    public function getPlatesBySector() {
-
-        while( array_shift($this->csv) && !empty($this->csv) ) {
-
-            list($sector, $description) = explode(',', $this->csv[0]);
-            
-            if( $sector == $this->sector ) {
-                array_push($this->plates, $description);
-            }
+    /**
+     * @return void
+     */
+    public function getPlates() {
+        if(!$this->getDiffSectores()) {
+            die("Don't exists differences" . PHP_EOL);
         }
 
-        if( $this->createCSV($this->sector, $this->plates) ) {
-            echo "Fichero creado con éxito";
+        if( $this->createCSV($this->sector, $this->plates_diff) ) {
+            echo "File created succesfully" . PHP_EOL;
+        } else {
+          echo "The file couldn't be created." . PHP_EOL;
         }
-
     }
 
+    /**
+     * @param $sector
+     * @param $plates
+     * @return bool
+     */
     protected function createCSV($sector, $plates) {
 
         // if file don't exists create it.
-        if( !file_exists(NAME_FILE) ) { 
+        if( !file_exists(NAME_FILE) ) {
             $newCSV = fopen(NAME_FILE, 'w');
-        } 
+        }
 
         // if file exists delete it and create a new one.
         if( unlink(NAME_FILE) ) {
             $newCSV = fopen(NAME_FILE, 'w');
         }
 
-        fputs($newCSV, "Sector, Matricula". PHP_EOL); 
+        fputs($newCSV, "Identificador, Descripción". PHP_EOL);
         foreach($plates as $plate) {
-            fputs($newCSV, $sector.','.$plate);
+            if($this->pmr) {
+                fputs($newCSV, $plate.',Matricula PMR '.$plate . PHP_EOL);
+            }else {
+                fputs($newCSV, $plate.',Residente autorizado sector '.$sector.' vehiculo '.$plate . PHP_EOL);
+            }
         }
         fclose($newCSV);
 
+        return (isset($newCSV)) ? true : false;
+    }
 
-        return true;
+    /**
+     * @param array $args
+     * @param array $cnf
+     * @return array|void
+     */
+    private function getCnf(array $args, array $cnf)
+    {
+        foreach ($args as $param) {
+
+            list($key, $value) = explode('=', $param);
+
+            if (empty($value)) {
+                die($key . ' is empty.');
+            }
+
+            $cnf[$key] = $value;
+        }
+        return $cnf;
+    }
+
+    /**
+     * @return bool|void
+     */
+    private function getDiffSectores() {
+
+        if($this->pmr) {
+            while (array_shift($this->csv) && !empty($this->csv)) {
+                $plate = preg_replace('/[^A-Za-z0-9]/', '', $this->csv[0]);
+                if(!empty($plate)) {
+                    array_push($this->plates, $plate);
+                }
+            }
+        }else {
+            while (array_shift($this->csv) && !empty($this->csv)) {
+
+                list($sector, $description) = explode(',', $this->csv[0]);
+                $plate = preg_replace('/[^A-Za-z0-9]/', '', $description);
+
+                if ($sector == $this->sector) {
+                    array_push($this->plates, $plate);
+                }
+            }
+        }
+
+        if (empty($this->plates)) {
+            die("There aren't license plates associated with that sector." . PHP_EOL);
+        }
+
+        while (array_shift($this->csv2) && !empty($this->csv2)) {
+            list($plates_smassa, $description) = explode(',', $this->csv2[0]);
+            array_push($this->smassa_plates, $plates_smassa);
+        }
+
+        if (!empty($this->plates) && !empty($this->smassa_plates)) {
+            $this->plates_diff = array_diff($this->plates, $this->smassa_plates);
+        }
+
+        return (isset($this->plates_diff)) ? true : false;
     }
 
 }
 
-$matriculas = new MatriculasCSV($argv);
-$matriculas->getPlatesBySector();
-
-?>
+$plates = new MatriculasCSV($argv);
+$plates->getPlates();
